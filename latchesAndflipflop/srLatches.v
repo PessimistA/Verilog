5de3c9c5ed00d80d3975684dbaaa4d3b56a9ec61
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-05-03 18:26:31 +0300

    Update srLatches.v

--- latchesAndflipflop/srLatches.v
+++ latchesAndflipflop/srLatches.v
@@ -17,20 +17,6 @@ nor m2(notq,s,q);
 
 endmodule
 
-//enable kodu
-module srlatchesenable(s, r, enable, q, notq);
-  input s, r, enable;     
-  output q, notq;         
-  wire s2,r2;   
-
-  nand (s2, s, enable);  
-  nand (r2, r, enable);   
-  nand (q, s2, notq);  
-  nand (notq, r2, q);   
-
-endmodule
-
-
 //testBench
 module testBench;
   reg s, r;
@@ -53,3 +39,51 @@ module testBench;
     $finish;
   end
 endmodule
+
+//enable kodu
+module srlatchesenable(s, r, enable, q, notq);
+  input s, r, enable;     
+  output q, notq;         
+  wire s2,r2;   
+
+  nand (s2, s, enable);  
+  nand (r2, r, enable);   
+  nand (q, s2, notq);  
+  nand (notq, r2, q);   
+
+endmodule
+//test bench for the enable
+module testBench;
+  reg s, r, enable;
+  wire q, notq;
+
+  srlatchesenable uut(s, r, enable, q, notq);  // SR latch with enable
+
+  initial begin
+    $monitor("Time=%0t | S=%b R=%b Enable=%b || Q=%b notQ=%b", 
+             $time, s, r, enable, q, notq);
+
+    // Başlangıç durumu
+    s = 0; r = 0; enable = 1; #10;
+
+    // Set durumu
+    s = 1; r = 0; enable = 1; #10;
+
+    // Reset durumu
+    s = 0; r = 1; enable = 1; #10;
+
+    // Hold durumu
+    s = 0; r = 0; enable = 1; #10;
+
+    // Enable = 0, çıkışlar değişmemeli
+    s = 1; r = 1; enable = 0; #10;
+
+    // Enable tekrar 1, çıkışlar güncellenmeli
+    enable = 1; #10;
+
+    // Testi bitir
+    $finish;
+  end
+endmodule
+
+
