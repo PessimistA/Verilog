6a087f83ef24bd0b7ab412c6d7f8c1efe5eed076
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-05-08 21:47:28 +0300

    Update TFlipFlop.v

--- latchesAndflipflop/TFlipFlop.v
+++ latchesAndflipflop/TFlipFlop.v
@@ -46,3 +46,35 @@ module tflipflop_tb1;
         $stop;
     end
 endmodule
+//mükemmel çalışan uzun hali
+module tflipflopnormal3(input t, input clk, output reg q1);
+    initial q1 = 0;  // <-- BURAYI EKLEDİK
+    always @(posedge clk) begin
+        case ({t,clk})
+            2'b00: q1 <= 0;        // Set
+            2'b01: q1 <= 1;  
+            2'b10: q1 <= 1; 
+            2'b11: q1 <= 0;    // Illegal state
+        endcase
+    end
+endmodule
+
+module tflipflop_tb12;
+    reg t, clk;
+    wire q1;
+
+    tflipflopnormal3 uut (.t(t), .clk(clk), .q1(q1));
+
+    initial begin
+        clk = 0;
+        forever #10 clk = ~clk;    // 10 birim periyot
+    end
+
+    initial begin
+        t = 0; #20;
+        t = 1; #20;
+        t = 0; #20;
+        t = 1; #20;
+        $stop;
+    end
+endmodule
