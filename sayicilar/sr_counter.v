58f63f4d23521bfb0ff6a5f0c4096b09edd6aa53
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-05-14 21:22:50 +0300

    Update sr_counter.v

--- sayicilar/sr_counter.v
+++ sayicilar/sr_counter.v
@@ -14,7 +14,7 @@ module sr_flip_flop (
                 2'b00: q <= q;          // No change
                 2'b01: q <= 0;       // Reset
                 2'b10: q <= 1;       // Set
-                2'b11:  q <= q; 
+                2'b11:  q <= 1'bx; 
             endcase
         end
     end
